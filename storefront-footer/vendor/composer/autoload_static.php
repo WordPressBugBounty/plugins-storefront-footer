674233984264ffa652a95e87bda7be6e2259b1db
Sean Murphy <sean@iamseanmurphy.com>
2024-10-14 15:02:55 +0000

    2.2.0

--- storefront-footer/vendor/composer/autoload_static.php
+++ storefront-footer/vendor/composer/autoload_static.php
@@ -51,6 +51,7 @@ class ComposerStaticInitd7667a28743049c2d17950f78bdc2ce9
 
     public static $classMap = array (
         'Automattic\\Jetpack\\Assets' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-assets/src/class-assets.php',
+        'Automattic\\Jetpack\\Assets\\Script_Data' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-assets/src/class-script-data.php',
         'Automattic\\Jetpack\\Assets\\Semver' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-assets/src/class-semver.php',
         'Automattic\\Jetpack\\Composer\\Manager' => __DIR__ . '/..' . '/automattic/jetpack-composer-plugin/src/class-manager.php',
         'Automattic\\Jetpack\\Composer\\Plugin' => __DIR__ . '/..' . '/automattic/jetpack-composer-plugin/src/class-plugin.php',
